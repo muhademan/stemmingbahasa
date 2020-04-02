7b1f096d504e74b324c4ef5cbad602e0e42a0b02
muhademan <60034198+muhademan@users.noreply.github.com>
2020-04-02 20:36:26 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+	$host='localhost';
+	$user='root';
+	$pass='';
+	$database='db_stemming-PPL';
+ 
+	$conn=mysql_connect($host,$user,$pass);
+	mysql_select_db($database);
+?>
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Tugas PPL</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-t-85 p-b-20">
+				<form  method="post" action="" class="login100-form validate-form">
+					
+					<font size="5"><b><center>Aplikasi Stemming Menggunakan Algoritma Nazief</center></b></font></br>
+					<font size="3"><b><center> Create by : </center></b></font>
+					<font size="2">Muhammad Ade Rahman &nbsp 1519250034 </font></br>
+					<font size="2">M. Syaiful Anam &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 1519250024 </font></br>
+					<font size="2">Ade Hendri Pandrean &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 1519250064 </font></br>
+					<font size="2">Rinalianti Ramadani &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 1620250502 </font></br>
+					<font size="2">Rendy Febya Ramadhan &nbsp&nbsp&nbsp 2012250092 </font></br>
+					</br>
+					
+					<span class="login100-form-avatar">
+						<img src="images/a.jpg" alt="AVATAR">
+					</span>
+
+					<div class="container-login100-form-btn">
+					<input class="login100-form-btn" type="button" value="Mulai Aplikasi" onclick="window.location = 'pencariankata.php';"/>				
+					</div>
+				
+					
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
